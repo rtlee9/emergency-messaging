8a7d88e4f61866898bfec315f92a758fd610319a
Ryan Lee <ryantlee9@gmail.com>
2019-04-10 22:37:31 -0700

    Create flow for student -> parent -> address

--- enrollment/students/views.py
+++ enrollment/students/views.py
@@ -5,7 +5,7 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
@@ -53,6 +53,26 @@ class AddressCreate(LoginRequiredMixin, CreateView):
     model = models.Address
     fields = ['address_1', 'address_2', 'city', 'state', 'zip_code']
 
+    def form_valid(self, form):
+        """ If coming from parent creation then associate this address with
+        that parent and return parent view.
+        """
+        parent_id = self.request.GET.get('parent_id')
+        if parent_id:
+            try:
+                parent = models.Parent.objects.get(pk=parent_id)
+            except models.Parent.DoesNotExist:
+                logger.warning(f'Parent ID {parent_id} does not exist ({self})')
+                return super().form_valid(form)
+            logger.debug(f'Parent for {self} is {parent}')
+            logger.debug(form.instance)
+            response = super().form_valid(form)
+            parent.address = form.instance
+            parent.save(update_fields=['address'])
+            redirect_url_base = reverse_lazy('students:parent-detail', kwargs={'pk': parent_id})
+            return HttpResponseRedirect(f'{redirect_url_base}?address_id={form.instance.pk}')
+        return super().form_valid(form)
+
 
 class AddressUpdate(LoginRequiredMixin, UpdateView):
     model = models.Address
@@ -76,10 +96,31 @@ class ParentCreate(LoginRequiredMixin, CreateView):
     model = models.Parent
     fields = ['first_name', 'last_name', 'email', 'phone_number', 'students', 'address']
 
+    def get_initial(self):
+        return {
+            'students': self.request.GET.getlist('student_id'),
+            'address': self.request.GET.getlist('address_id'),
+        }
+
+    def form_valid(self, form):
+        """ If address is empty then redirect to address creation.
+        """
+        response = super().form_valid(form)
+        if not form.data['address']:
+            redirect_url_base = reverse_lazy('students:address-add')
+            return HttpResponseRedirect(f'{redirect_url_base}?parent_id={form.instance.pk}')
+        else:
+            return response
+
 
 class ParentUpdate(LoginRequiredMixin, UpdateView):
     model = models.Parent
     fields = ['first_name', 'last_name', 'email', 'phone_number', 'students', 'address']
+
+    def get_initial(self):
+        return {
+            'students': self.request.GET.getlist('student_id'),
+        }
 
 
 class ParentDelete(LoginRequiredMixin, DeleteView):
@@ -122,12 +163,12 @@ def sms_response(request):
         return HttpResponse(str(resp))
 
     for to_number in parent_phone_numbers:
-        message = client.messages.create(
+        twilio_message = client.messages.create(
             body=message,
             from_='+17472394729',
             to=to_number,
         )
-        logger.debug(f'Twilio message to {to_number} SID {message.sid}')
+        logger.debug(f'Twilio message to {to_number} SID {twilio_message.sid}')
 
     return HttpResponse(str(resp))
 
