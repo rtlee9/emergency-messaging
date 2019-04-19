1a127059e44264d2aa497ce374fd7bbf26438247
Ryan Lee <ryantlee9@gmail.com>
2019-04-19 09:01:35 -0700

    Add url tests for parents

--- enrollment/students/tests/test_urls.py
+++ enrollment/students/tests/test_urls.py
@@ -8,13 +8,25 @@ pytestmark = pytest.mark.django_db
 
 
 def test_parent_detail(parent: models.Parent):
-    assert (
-        reverse("students:parent-detail", kwargs={"pk": parent.pk})
-        == f"/parents/{parent.pk}/"
-    )
+    assert reverse("students:parent-detail", kwargs={"pk": parent.pk}) == f"/parents/{parent.pk}/"
     assert resolve(f"/parents/{parent.pk}/").view_name == "students:parent-detail"
 
 
 def test_list():
     assert reverse("students:parent-list") == "/parents/"
     assert resolve("/parents/").view_name == "students:parent-list"
+
+
+def test_update(parent: models.Parent):
+    assert reverse("students:parent-update", kwargs={'pk': parent.pk}) == f"/parents/{parent.pk}/update/"
+    assert "students:parent-update" == resolve(f"/parents/{parent.pk}/update/").view_name
+
+
+def test_delete(parent: models.Parent):
+    assert reverse("students:parent-delete", kwargs={'pk': parent.pk}) == f"/parents/{parent.pk}/delete/"
+    assert "students:parent-delete" == resolve(f"/parents/{parent.pk}/delete/").view_name
+
+
+def test_add(parent: models.Parent):
+    assert reverse("students:parent-add") == f"/parents/add/"
+    assert "students:parent-add" == resolve(f"/parents/add/").view_name
