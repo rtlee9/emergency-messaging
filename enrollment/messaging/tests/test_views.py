51247cafa6a47769ca1330c72dc610aeadc384d7
Ryan Lee <ryantlee9@gmail.com>
2019-04-24 08:24:11 -0700

    Refactor message view tests

--- enrollment/messaging/tests/test_views.py
+++ enrollment/messaging/tests/test_views.py
@@ -78,6 +78,12 @@ class TestSmsView:
         views.client = TwilioTestClient(
             settings.TWILIO_SID, settings.TWILIO_TOKEN)
 
+    def _setup_sites(self):
+        # create batch of sites and classrooms
+        self.sites = SiteFactory.create_batch(self.ns)
+        self.classrooms = ClassroomFactory.create_batch(self.nc)
+        self.parents = self._gen_all_families()
+
     def test_no_pin(
         self,
         message: models.Message,
@@ -139,50 +145,33 @@ class TestSmsView:
         parents.extend(self._gen_family(3))
         return parents
 
-    def test_good_message(
-        self,
-        message: models.Message,
-        message_status: models.MessageStatus,
-        request_factory: RequestFactory,
-    ):
-        msg_og = message.body
-        msg_og_trunc = msg_og[:-len(settings.SMS_PIN)]
-        message.body = settings.SMS_PIN + msg_og_trunc
+    def clear_client(fn):
+        def wrapper(*args, **kwargs):
+            response = fn(*args, **kwargs)
+            views.client.messages.created = []
+            return response
+        return wrapper
 
-        # create batch of sites
-        sites = SiteFactory.create_batch(self.ns)
-        classrooms = ClassroomFactory.create_batch(self.nc)
-
-        # create batch of parents
-        parents = self._gen_all_families()
-
-        # send request
-        request = request_factory.post(
-            "/sms/", self._construct_data(message))
-        response = self.view(request)
-        self._test_empty_response(response)
-
-        # check for site prompt
+    @clear_client
+    def _check_parse_site(self):
+        """Check site prompt and respond with random selection."""
         msgs = views.client.messages.created
+        # check twilio test client sent the right message
         assert len(msgs) == 1
         assert msgs[0]['body'].startswith('Please select a site')
         assert len(msgs[0]['body'].split('\n')) == self.ns + 1
+        # parse site options
         site_options = [
             full_option.split(':')[0][1:-1]
             for full_option
             in msgs[0]['body'].split('\n')[1:]
         ]
-        views.client.messages.created = []
-
-        # choose arbitrary site
         site_choice = choice(site_options)
-        message = MessageFactory(body=f'{site_choice}', from_phone_number=message.from_phone_number)  # TODO: test user input errors
-        request = request_factory.post(
-            "/sms/", self._construct_data(message))
-        response = self.view(request)
-        self._test_empty_response(response)
+        return site_choice
 
-        # check test client for confirmation message
+    @clear_client
+    def _test_distribution(self, site_choice, from_phone_number, expected_msg):
+        """Check test client for confirmation message."""
         msgs = views.client.messages.created
         n = Parent.objects.\
             filter(students__classroom__site__pk=int(site_choice)).\
@@ -190,75 +179,75 @@ class TestSmsView:
         assert len(msgs) == 1 + n
         # last message is confirmation
         assert msgs[-1]['to'] == phonenumbers.format_number(
-            message.from_phone_number, phonenumbers.PhoneNumberFormat.E164)
+            from_phone_number, phonenumbers.PhoneNumberFormat.E164)
         assert msgs[-1]['body'].lower().startswith('your message has been sent')
         # check messages to all parents
         for i in range(n - 1):
             msg = msgs[i]
             parent = Parent.objects.get(phone_number=msg['to'])
-            assert msg['body'] == msg_og_trunc
+            assert msg['body'] == expected_msg
             assert msg['to'] == phonenumbers.format_number(
                 parent.phone_number, phonenumbers.PhoneNumberFormat.E164)
-        views.client.messages.created = []
 
-        # subsequent request shouldn't require PIN
-        message = MessageFactory(body=msg_og, from_phone_number=message.from_phone_number)
+    def _handle_respond_site_prompt(self, from_phone_number, request_factory, site_choice=None):
+        if site_choice:
+            _ = self._check_parse_site()
+        else:
+            site_choice = self._check_parse_site()
+        prompt_response = MessageFactory(
+            body=f'{site_choice}',
+            from_phone_number=from_phone_number,
+        )
+        request = request_factory.post(
+            "/sms/", self._construct_data(prompt_response))
+        response = self.view(request)
+        self._test_empty_response(response)
+        return site_choice
+
+    def _request_check_empty(self, message, request_factory):
+        """Send message as POST request, assert empty response."""
         request = request_factory.post(
             "/sms/", self._construct_data(message))
         response = self.view(request)
         self._test_empty_response(response)
 
-        # check for site prompt [COPY]
-        msgs = views.client.messages.created
-        assert len(msgs) == 1
-        assert msgs[0]['body'].startswith('Please select a site')
-        assert len(msgs[0]['body'].split('\n')) == self.ns + 1
-        site_options = [
-            full_option.split(':')[0][1:-1]
-            for full_option
-            in msgs[0]['body'].split('\n')[1:]
-        ]
-        views.client.messages.created = []
-
-        # choose arbitrary site [COPY]
-        site_choice = choice(site_options)
-        message = MessageFactory(body=f'{site_choice}', from_phone_number=message.from_phone_number)  # TODO: test user input errors
-        request = request_factory.post(
-            "/sms/", self._construct_data(message))
-        response = self.view(request)
-        self._test_empty_response(response)
-
-        # check test client for confirmation message [COPY]
-        msgs = views.client.messages.created
-        n = Parent.objects.\
-            filter(students__classroom__site__pk=int(site_choice)).\
-            distinct().count()
-        assert len(msgs) == 1 + n
-        # last message is confirmation
-        assert msgs[-1]['to'] == phonenumbers.format_number(
-            message.from_phone_number, phonenumbers.PhoneNumberFormat.E164)
-        assert msgs[-1]['body'].lower().startswith('your message has been sent')
-        # check messages to all parents
-        for i in range(n - 1):
-            msg = msgs[i]
-            parent = Parent.objects.get(phone_number=msg['to'])
-            assert msg['body'] == msg_og
-            assert msg['to'] == phonenumbers.format_number(
-                parent.phone_number, phonenumbers.PhoneNumberFormat.E164)
-        views.client.messages.created = []
-
-        # subsequent request should require PIN if after timeout
+    @clear_client
+    def _test_pin_timeout(self, from_phone_number, request_factory):
+        """Test PIN timeout. Expecting a SMS response indicating incorrect PIN."""
         msgs = views.client.messages.created
         time.sleep(settings.AUTH_SECONDS + 1)
-        message = MessageFactory(body=msg_og, from_phone_number=message.from_phone_number)
-        request = request_factory.post(
-            "/sms/", self._construct_data(message))
-        response = self.view(request)
-        self._test_empty_response(response)
+        message = MessageFactory(from_phone_number=from_phone_number)
+        self._request_check_empty(message, request_factory)
         # check test client for confirmation message
         msgs = views.client.messages.created
         assert len(msgs) == 1
         # last message is confirmation
         assert msgs[-1]['to'] == message.from_phone_number
         assert msgs[-1]['body'] == 'Incorrect PIN'
-        views.client.messages.created = []
+
+    def test_good_message(
+        self,
+        message: models.Message,
+        message_status: models.MessageStatus,
+        request_factory: RequestFactory,
+    ):
+        self._setup_sites()  # setup
+        # construct message with PIN
+        msg_og = message.body
+        msg_og_trunc = msg_og[:-len(settings.SMS_PIN)]
+        message.body = settings.SMS_PIN + msg_og_trunc
+        from_phone_number = message.from_phone_number
+
+        # check site prompt, choose arbitrary site, confirm distribution
+        self._request_check_empty(message, request_factory)
+        site_choice = self._handle_respond_site_prompt(from_phone_number, request_factory)
+        self._test_distribution(site_choice, from_phone_number, msg_og_trunc)
+
+        # subsequent request shouldn't require PIN
+        message = MessageFactory(from_phone_number=from_phone_number)
+        self._request_check_empty(message, request_factory)
+        site_choice = self._handle_respond_site_prompt(from_phone_number, request_factory)
+        self._test_distribution(site_choice, from_phone_number, message.body)
+
+        # subsequent request should require PIN if after timeout
+        self._test_pin_timeout(from_phone_number, request_factory)
